c212d7d72901d56c5ee8d0d9ac2467e8d1ee1a45
Chargk <mthwhsn@gmail.com>
2025-02-17 03:34:13 +0200

    building the landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className="min-h-screen h-screen overflow-hidden flex flex-col">
           {children}
         </body>
       </html>
